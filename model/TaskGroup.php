5036f437b258a3ebcd1c08e9938ed8a9eea327e7
cashsun <mr.cashsun@gmail.com>
2012-12-16 21:59:33 +0000

    add priority to Group

--- model/TaskGroup.php
+++ model/TaskGroup.php
@@ -5,11 +5,16 @@ include_once '../db/Task.php';
 class TaskGroup{
     public $gid;
     private $title;
+    /**
+     *  array of Task
+     * @var Task
+     */
     private $tasks;
+    private $priority;
     
     function __construct($gid,$groupInfo) {	
         $this->gid = $gid;
-        
+        $this->priority = $groupInfo['priority'];
         $this->title = $groupInfo['title'];
         $this->tasks = $groupInfo['tasks'];
     }
