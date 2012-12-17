d50dd0a15d9e3791745d49705c836f478727a0f2
Cash Sun <mr.cashsun@gmail.com>
2012-12-17 22:50:24 +0000

    GUI

--- view/main_panel.php
+++ view/main_panel.php
@@ -1,15 +1,19 @@
 <?php 
 //uid by SESSION
-if(isset($_GET['uid']))
+if(isset($_GET['uid'])){
     $user = getAllByUid($_GET['uid']);
+}
 ?>
 <div id="master">
     <div id="navibar">
-        
+        <img id="profile_image" src="image/profile.png" alt=""/>
+        <div id="profile_username" class="username"><?php echo $user->getUsername() ?></div>
     </div>
     <div id="panel_main">
         <div id="panal_group">
-            <center><h1>Groups</h1></center>
+            <ul>
+                
+            </ul>
         </div>
         <div id="panal_task">
             <center><h1>tasks</h1></center>
