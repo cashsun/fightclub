8bc4f77c9f38e73cecac4e64ffec386467338aba
Qianze Zhang <qianze.zhang@gmail.com>
2013-01-09 21:41:05 +0000

    hehe

--- service/createTask.php
+++ service/createTask.php
@@ -3,7 +3,10 @@ include_once '../db/DBadapter.php';
 if(isset($_POST['uid'])&&isset($_POST['tgid'])&&isset($_POST['content'])){
     $db = new DBadapter();
     $db->connect();
-    $result = $db->createTask($_POST['uid'],$_POST['otid'],$_POST['tgid'],$_POST['content']);
+    if(!isset($_POST['otid']))
+      $result = $db->createOriTask($_POST['uid'],$_POST['tgid'],$_POST['content']);
+    else
+      $result = $db->createOriTask($_POST['uid'],$_POST['otid'],$_POST['tgid'],$_POST['content']);
     echo $result;
 }
 ?>
