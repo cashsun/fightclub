b4179087348151c4b916ce8a5ae87d41a1bc139e
Cash Sun <mr.cashsun@gmail.com>
2013-01-09 21:50:36 +0000

    hehe

--- header.php
+++ header.php
@@ -2,11 +2,8 @@
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
         <title>Fightclub</title>
         <link rel="shortcut icon" href="favicon.ico" />
+        <link rel="stylesheet" href="css/pepper-grinder/jquery-ui-1.8.23.custom.css" type="text/css"/>
         <link rel="stylesheet" href="css/main.css"/>
         <script type="text/javascript" src="js/jquery-1.8.1.min.js"></script>
-        <script type="text/javascript" src="js/jquery-ui-1.8.23.custom.min.js"></script>
-<!--        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js"></script>
-        <script>!window.jQuery && document.write(unescape('%3Cscript src="js/jquery-1.8.1.min.js"%3E%3C/script%3E'))</script>
-        <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js"></script>
-        <script>!window.jQuery.ui && document.write(unescape('%3Cscript src="js/jquery-ui-1.8.23.custom.min.js"%3E%3C/script%3E'))</script>-->
+        <script type="text/javascript" src="js/jquery-ui-1.9.2.custom.min.js"></script>
     </head>
