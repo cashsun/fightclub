92a2ccfc895f9482b689849f9aec1e886c6af4e0
Qianze Zhang <qianze.zhang@gmail.com>
2013-03-02 04:02:15 +0000

    for mr xxx

--- test/regTester.php
+++ test/regTester.php
@@ -1,6 +1,6 @@
 <?php
-preg_match_all("/@\w*/",
-    "@cashsun @plutoless @testuser",
+preg_match_all("/@[a-zA-Z0-9.]*/",
+    "@cashsun @plutoless @test.user",
     $out, PREG_PATTERN_ORDER);
 $atstr = implode(",", $out[0]);
             $atstr = str_replace("@", "", $atstr);
